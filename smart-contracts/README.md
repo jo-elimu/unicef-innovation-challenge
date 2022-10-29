99dd40df3a2cac923c76bdbf771f2528ae272884
jo-elimu <1451036+jo-elimu@users.noreply.github.com>
2022-10-29 22:24:15 +0800

    Deploy to testnet

--- smart-contracts/README.md
+++ smart-contracts/README.md
@@ -18,17 +18,21 @@ npx hardhat coverage
 ## Deployment Instructions
 
 ```shell
+cp .env.example .env
+```
+
+```shell
 npx hardhat run scripts/deploy.ts
 ```
 
 Deploy to testnet:
 
 ```shell
-hardhat run --network ropsten scripts/deploy.ts
+npx hardhat run --network goerli scripts/deploy.ts
 ```
 
 Etherscan verification:
 
 ```shell
-npx hardhat verify --network ropsten DEPLOYED_CONTRACT_ADDRESS "Hello, Hardhat!"
+npx hardhat verify --network goerli DEPLOYED_CONTRACT_ADDRESS "Hello"
 ```
--- smart-contracts/test/Greeter.ts
+++ smart-contracts/test/Greeter.ts
@@ -4,16 +4,16 @@ import { ethers } from "hardhat";
 describe("Greeter", function () {
   it("Should return the new greeting once it's changed", async function () {
     const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
+    const greeter = await Greeter.deploy("Hello");
     await greeter.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    expect(await greeter.greet()).to.equal("Hello");
 
-    const setGreetingTx = await greeter.setGreeting("¡Hola, mundo!");
+    const setGreetingTx = await greeter.setGreeting("Hola");
 
     // Wait until the transaction is mined
     await setGreetingTx.wait();
 
-    expect(await greeter.greet()).to.equal("¡Hola, mundo!");
+    expect(await greeter.greet()).to.equal("Hola");
   });
 });
